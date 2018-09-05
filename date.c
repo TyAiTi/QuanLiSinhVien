82491446923b2352b58fa560a8308552edd371fa
TyAiTi <35554335+TyAiTi@users.noreply.github.com>
2018-09-05 20:34:30 +0700

    Em biên dịch và chạy được ạ, anh kiểm tra giúp em

--- date.c
+++ date.c
@@ -4,8 +4,22 @@
 
 void input_date(_date *date) {
 	printf("Implement input_date here!\n");
+	printf("date/month/year\n");
+	scanf("%d %d %d", &date->date, &date->month, &date->year);
 }
 
 void print_date(_date date) {
 	printf("Implement print_date here!s\n");
+	printf("date/month/year\n");
+	printf("%d/%d/%d", date.date, date.month, date.year);
+
+}
+
+int main()
+{
+	_date date;
+	input_date(&date);
+	print_date(date);
+
+	return 0;
 }
